1c2d443ded37e1f99f517b237c25095b35762303
taesungs <70329288+taesungs@users.noreply.github.com>
2026-03-28 21:07:36 +0900

    init

---
+++ src/main/java/com/example/be_young04/BeYoung04Application.java
@@ -0,0 +1,13 @@
+package com.example.be_young04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeYoung04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeYoung04Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/be_young04/BeYoung04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_young04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeYoung04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
