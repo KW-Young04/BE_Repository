f139fc0d3105bc1d9d10a08ebce877d04d022c01
taesungs <70329288+taesungs@users.noreply.github.com>
2026-08-24 14:21:17 +0900

    feat: 프론트엔드 배포주소 추가

--- src/main/java/com/example/be_young04/global/config/SecurityConfig.java
+++ src/main/java/com/example/be_young04/global/config/SecurityConfig.java
@@ -84,7 +84,8 @@ public class SecurityConfig {
         configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
                 "http://localhost:8080",
-                "http://127.0.0.1:8080"
+                "http://127.0.0.1:8080",
+                "https://fe-repository-coral.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
